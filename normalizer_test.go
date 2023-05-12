752c4332c402445fee76360c5f20bf05ec2a37ea
shaurya947 <shaurya.arora1@gmail.com>
2023-05-12 11:09:16 -0400

    Use testCase struct

--- normalizer_test.go
+++ normalizer_test.go
@@ -6,28 +6,31 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type normalizeTestCase struct {
+	in, out   string
+	shouldErr bool
+}
+
 func TestNormalizePhone(t *testing.T) {
-	normalized, err := normalizePhone("")
-	require.Empty(t, normalized)
-	require.NotNil(t, err)
+	testCases := []normalizeTestCase{
+		{in: "", out: "", shouldErr: true},
+		{in: "  4", out: "", shouldErr: true},
+		{in: "  4", out: "", shouldErr: true},
+		{in: "1234567890", out: "1234567890", shouldErr: false},
+		{in: "(123)456-7892", out: "1234567892", shouldErr: false},
+		{in: "(  123) 456-78 93", out: "1234567893", shouldErr: false},
+		{in: "4(123)456-7892", out: "", shouldErr: true},
+	}
 
-	normalized, err = normalizePhone("  4")
-	require.Empty(t, normalized)
-	require.NotNil(t, err)
-
-	normalized, err = normalizePhone("1234567890")
-	require.Equal(t, "1234567890", normalized)
-	require.Nil(t, err)
-
-	normalized, err = normalizePhone("(123)456-7892")
-	require.Equal(t, "1234567892", normalized)
-	require.Nil(t, err)
-
-	normalized, err = normalizePhone("(  123) 456-78 93")
-	require.Equal(t, "1234567893", normalized)
-	require.Nil(t, err)
-
-	normalized, err = normalizePhone("4(123)456-7892")
-	require.Empty(t, normalized)
-	require.NotNil(t, err)
+	for _, tc := range testCases {
+		t.Run(tc.in, func(t *testing.T) {
+			normalized, err := normalizePhone(tc.in)
+			require.Equal(t, tc.out, normalized)
+			if tc.shouldErr {
+				require.NotNil(t, err)
+			} else {
+				require.Nil(t, err)
+			}
+		})
+	}
 }
